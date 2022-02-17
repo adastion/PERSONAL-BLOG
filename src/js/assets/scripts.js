8ee79998700dc94ebb34598fe8f08b4dab7ec1f9
Andrey <andrey.72006@gmail.com>
2022-02-18 01:20:45 +0300

    Test

---
+++ src/js/assets/scripts.js
@@ -0,0 +1,8 @@
+var burgerBtn = document.querySelector('.navigation__burger'),
+    sideBar = document.querySelector('.sidebar')
+
+burgerBtn.addEventListener('click', function (evt) {
+  evt.preventDefault()
+  sideBar.classList.toggle('active-sidebar')
+})
+
